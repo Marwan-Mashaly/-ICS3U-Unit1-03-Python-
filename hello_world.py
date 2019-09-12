f2716a1649fcda67c1c63903fcc47b1bc32b7fbd
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-09-12 17:15:24 +0000

    added hello world program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Marwan Mashaly
+# Created on: September 2019 
+# this is the hello world program 
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
